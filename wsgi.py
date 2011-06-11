6f5de27115e6c92b4019aace48c5ef0525e52aca
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 14:34:09 -0700

    local/deploy settings is how it is done

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'deploy_settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
