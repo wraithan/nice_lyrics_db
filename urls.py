74b640c7395905c71700f8a77c538e454abd3369
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 12:48:55 -0700

    Initial commit

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'nice_lyrics_db.views.home', name='home'),
+    # url(r'^nice_lyrics_db/', include('nice_lyrics_db.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
