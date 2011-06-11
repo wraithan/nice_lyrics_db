7e6fff6f3a707670e49c1438b630b1be8316a960
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 15:12:24 -0700

    urls

--- nice_lyrics_db/settings.py
+++ nice_lyrics_db/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'nice_lyrics_db.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
