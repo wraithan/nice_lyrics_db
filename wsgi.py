53e63757eb84fcc95f874559cc6568ccba3e7491
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 14:45:41 -0700

    deploy_settings are good

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'deploy_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nice_lyrics_db.deploy_settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
