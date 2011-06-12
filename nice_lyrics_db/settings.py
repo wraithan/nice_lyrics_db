1cc31327d3fb04cab2d03e176b03e33a65e6124f
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 18:33:18 -0700

    workie workie

--- nice_lyrics_db/settings.py
+++ nice_lyrics_db/settings.py
@@ -105,6 +105,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'music',
+    'django_extensions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
