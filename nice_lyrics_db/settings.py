bcc9637eca0568e300c085c0bbd411f8c528609d
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 17:29:13 -0700

    installed_apps is important

--- nice_lyrics_db/settings.py
+++ nice_lyrics_db/settings.py
@@ -104,6 +104,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'music',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
