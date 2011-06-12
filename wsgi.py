a98dcde12a44890c23f45cea5293661840e9fcfa
Chris McDonald <XWraithanX@gmail.com>
2011-06-12 01:11:04 -0700

    using recommended django wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nice_lyrics_db.deploy_settings'
-
 import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+djangoapplication = django.core.handlers.wsgi.WSGIHandler()
+def application(environ, start_response):
+    if 'SCRIPT_NAME' in environ:
+        del environ['SCRIPT_NAME']
+    return djangoapplication(environ, start_response)
