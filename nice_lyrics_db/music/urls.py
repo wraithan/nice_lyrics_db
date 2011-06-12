f7d50f5fb961169c6e83a72abb7be49dcc855562
Chris McDonald <XWraithanX@gmail.com>
2011-06-11 17:26:09 -0700

    home page skel

--- nice_lyrics_db/music/urls.py
+++ nice_lyrics_db/music/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from views import home
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include(music.urls)),
+    url(r'^$', home, name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- nice_lyrics_db/music/views.py
+++ nice_lyrics_db/music/views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from flameforged.view_helpers import render_to
+
+def home(request):
+    
